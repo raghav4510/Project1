2ebc3ca515c14e22da688a64abdf6b10970bf43d
raghav4510 <personaljob4510@gmail.com>
2022-06-22 13:47:46 +0530

    Second commit

---
+++ func2.py
@@ -0,0 +1,10 @@
+def add(x,y):
+    return x+y
+def sub(x,y):
+    return x - y
+
+x = int(input("Enter value of x :"))
+y = int(input("Enter value of y :"))
+
+print("adding x and y :", add(x,y))
+print("difference of  x and y :", sub(x,y)))
