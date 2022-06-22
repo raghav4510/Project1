00ba7cf4e0ecb9f64a32bce893eda00f987d2796
raghav4510 <personaljob4510@gmail.com>
2022-06-22 13:50:43 +0530

    commit to modify second commit

--- func2.py
+++ func2.py
@@ -7,4 +7,4 @@ x = int(input("Enter value of x :"))
 y = int(input("Enter value of y :"))
 
 print("adding x and y :", add(x,y))
-print("difference of  x and y :", sub(x,y)))
+print("difference of  x and y :", sub(x,y))
